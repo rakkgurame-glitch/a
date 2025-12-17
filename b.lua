67504504957a47fef727aa4d17849845ff978c20
rakkgurame-glitch <rakkgurame@gmail.com>
2025-12-18 02:31:59 +0700

    Update URL for top English nouns list

--- b.lua
+++ b.lua
@@ -111,7 +111,7 @@ local function LoadWords()
 
     local ok, result = pcall(function()
         local res = reqFunc({
-            Url = "https://raw.githubusercontent.com/david47k/top-english-wordlists/refs/heads/master/top_english_nouns_lower_500000.txt", -- ganti ini dengan link file .txt kamu
+            Url = "https://raw.githubusercontent.com/rakkgurame-glitch/a/refs/heads/main/wlist_match1.txt", -- ganti ini dengan link file .txt kamu
             Method = "GET"
         })
         return (type(res) == "table" and res.Body) or res
